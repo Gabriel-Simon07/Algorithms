ab1c530fe7843e94a2c28ce460aebcf3b87d1de3
Gabriel Simon <gabrielsimon775@gmail.com>
2022-09-07 12:59:01 -0300

    Chess game

---
+++ src/chess/Piece.java
@@ -0,0 +1,14 @@
+package chess;
+
+public class Piece {
+	protected Position position;
+	protected Board board;
+
+	public Piece(Board board) {
+		this.board = board;
+	}
+	
+	protected Board getBoard() {
+		return board;
+	}
+}
